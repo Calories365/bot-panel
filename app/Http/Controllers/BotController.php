797ac5c8c02d224471fa26524bc67f295934dddc
Maaaaxim <maxim.kubichka@gmail.com>
2024-12-14 23:08:37 +0200

    building commit

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -104,6 +104,7 @@ class BotController extends BaseController
     public function handle(TelegramHandler $telegramHandler, $bot, Request $request)
     {
         try {
+            Log::info('BotController::handle');
             $telegramHandler->handle($bot, $request);
             return response()->json(['status' => 'success']);
         } catch
