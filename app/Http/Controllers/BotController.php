c2779cc578e9b7be3394c6f2ae626c53fcc19565
Maaaaxim <maxim.kubichka@gmail.com>
2025-01-06 13:24:23 +0200

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -29,7 +29,7 @@ class BotController extends BaseController
     {
         $this->botManagmentService = $botManagmentService;
     }
-
+//123
     public function index(Request $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
         $perPage = $request->input('per_page', 10);
